9646106ac08b42c2e26e36bed4886d56831e63e0
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2025-02-06 15:53:18 +0200

    navbar fix toggle commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import logo from '../../assets/images/freshcart-logo.svg';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../Context/AuthContextProvider';
@@ -6,13 +6,18 @@ import { AuthContext } from '../../Context/AuthContextProvider';
 export default function Navbar() {
     let { token, setToken } = useContext(AuthContext);
     let navg = useNavigate();
-
+    let button = document.getElementById("button")
+    let menu = document.getElementById('menu')
     function logout() {
         localStorage.removeItem('token');
         setToken(null);
         navg('/');
     }
-
+    function toggleMenu() {
+        button.addEventListener('click', () => {
+            menu.classList.toggle('hidden')
+        })
+    }
 
     return (
 
@@ -25,11 +30,7 @@ export default function Navbar() {
                         <Link to="/home" className="me-10  ">
                             <img src={logo} className="h-8" alt="freshCart" />
                         </Link>
-
-
-                        {/* ************** TABS******************** */}
-
-                        {token ? <div className="hidden  md:flex md:justify-between w-full gap-80 absolute left-0 top-10 md:relative md:top-0" id="navbar-default">
+                        {token ? <div className=" hidden  md:flex md:justify-between w-full gap-80 absolute left-0 top-12 md:relative md:top-0" id="menu">
                             <ul className="font-medium  text-xl flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row  gap-5  md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                                 <li>
                                     <NavLink to="/home" className=" hover:cursor-pointer hover:text-active duration-150 block py-2 px-3 bg-active-700 rounded md:bg-transparent md:text-active-700 md:p-0 dark:text-white md:dark:text-active-500">Home</NavLink>
@@ -109,7 +110,7 @@ export default function Navbar() {
                         </ul>
                     </div>
 
-                    <button data-dropdown-toggle="navbar-default" type="button" className="inline-flex items-center ml-3 p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                    <button onClick={toggleMenu} id='button' data-dropdown-toggle="menu" type="button" className="inline-flex items-center ml-3 p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="menu" aria-expanded="false">
                         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
                         </svg>
