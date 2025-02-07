1865e90bb6161377730cf40a3cf6e8fce1a7afb2
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2025-02-07 21:23:10 +0200

    flex for login commit

--- src/components/ForgetPassword/ForgetPassword.jsx
+++ src/components/ForgetPassword/ForgetPassword.jsx
@@ -65,73 +65,77 @@ export default function ForgetPassword() {
     }
 
     return (<>
+        <div className="h-screen flex justify-center items-center w-full">
 
-        {formDisplay ? <div>
-            <h2 className="text-center font-bold py-6 text-3xl">Verify your email</h2>
+            {formDisplay ? <div className="w-full">
+                <h2 className="text-center font-bold py-6 text-3xl">Verify your email</h2>
 
-            <form onSubmit={forgetForm.handleSubmit} className="max-w-sm mx-auto px-3">
-                <div className="mb-5">
-                    <label
-                        htmlFor="email"
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                        Your email
-                    </label>
-                    <input
-                        value={forgetForm.values.email}
-                        onChange={forgetForm.handleChange}
-                        onBlur={forgetForm.handleBlur}
-                        type="email"
-                        id="email"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    />
-                    {forgetForm.touched.email && forgetForm.errors.email ? (
-                        <p className="text-red-600">{forgetForm.errors.email}</p>
-                    ) : (
-                        ""
-                    )}
-                </div>
-                <button
-                    type="submit"
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                    Send
-                </button>
-            </form>
-        </div>
-            : <div>
-                {/* <h2 className="text-center font-bold py-6 text-3xl">Enter reset code</h2> */}
-
-                <form onSubmit={verifyResetCodeForm.handleSubmit} className="max-w-sm mx-auto px-3 mt-8">
+                <form onSubmit={forgetForm.handleSubmit} className="max-w-sm mx-auto px-3">
                     <div className="mb-5">
                         <label
-                            htmlFor="resetCode"
+                            htmlFor="email"
                             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                         >
-                            Enter reset code
+                            Your email
                         </label>
                         <input
-                            value={verifyResetCodeForm.values.resetCode}
-                            onChange={verifyResetCodeForm.handleChange}
-                            onBlur={verifyResetCodeForm.handleBlur}
-                            type="string"
-                            id="resetCode"
-                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            value={forgetForm.values.email}
+                            onChange={forgetForm.handleChange}
+                            onBlur={forgetForm.handleBlur}
+                            type="email"
+                            id="email"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-active focus:border-active block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         />
-                        {verifyResetCodeForm.touched.resetCode && verifyResetCodeForm.errors.resetCode ? (
-                            <p className="text-red-600">{verifyResetCodeForm.errors.resetCode}</p>
+                        {forgetForm.touched.email && forgetForm.errors.email ? (
+                            <p className="text-red-600">{forgetForm.errors.email}</p>
                         ) : (
                             ""
                         )}
                     </div>
                     <button
                         type="submit"
-                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        className="text-white bg-active hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
-                        Verify Code
+                        Send
                     </button>
                 </form>
-            </div>}
+            </div>
+                : <div className="w-full">
+                    <h2 className="text-center font-bold  text-3xl">Enter reset code</h2>
+
+                    <form onSubmit={verifyResetCodeForm.handleSubmit} className="max-w-sm mx-auto px-3 mt-8">
+                        <div className="mb-5">
+                            <label
+                                htmlFor="resetCode"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            >
+                                Enter reset code
+                            </label>
+                            <input
+                                value={verifyResetCodeForm.values.resetCode}
+                                onChange={verifyResetCodeForm.handleChange}
+                                onBlur={verifyResetCodeForm.handleBlur}
+                                type="string"
+                                id="resetCode"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-active focus:border-active block w-full p-2.5  dark:focus:ring-active dark:focus:border-active"
+                            />
+                            {verifyResetCodeForm.touched.resetCode && verifyResetCodeForm.errors.resetCode ? (
+                                <p className="text-red-600">{verifyResetCodeForm.errors.resetCode}</p>
+                            ) : (
+                                ""
+                            )}
+                        </div>
+                        <button
+                            type="submit"
+                            className="text-white bg-active hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        >
+                            Verify Code
+                        </button>
+                    </form>
+                </div>}
+
+        </div>
+
 
 
 
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -47,61 +47,63 @@ export default function Login() {
   }
 
   return (
-    <div className="mt-16 ">
-      <h2 className="text-center font-bold py-6 text-3xl">Login Now</h2>
-
-      <form onSubmit={LoginForm.handleSubmit} className="max-w-sm mx-auto px-3">
-        <div className="mb-5">
-          <label
-            htmlFor="email"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+    <div className="w-full h-screen flex justify-center items-center ">
+    <div className="w-full">
+        <h2 className="text-center font-bold py-6 text-3xl">Login Now</h2>
+      
+        <form onSubmit={LoginForm.handleSubmit} className="max-w-sm mx-auto px-3">
+          <div className="mb-5">
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Your email
+            </label>
+            <input
+              value={LoginForm.values.email}
+              onChange={LoginForm.handleChange}
+              onBlur={LoginForm.handleBlur}
+              type="email"
+              id="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-active focus:border-active block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+            {LoginForm.touched.email && LoginForm.errors.email ? (
+              <p className="text-red-600">{LoginForm.errors.email}</p>
+            ) : (
+              ""
+            )}
+          </div>
+      
+          <div className="mb-5">
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Your password
+            </label>
+            <input
+              value={LoginForm.values.password}
+              onChange={LoginForm.handleChange}
+              onBlur={LoginForm.handleBlur}
+              type="password"
+              id="password"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-active focus:border-active block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+            {LoginForm.touched.password && LoginForm.errors.password ? (
+              <p className="text-red-600">{LoginForm.errors.password}</p>
+            ) : (
+              ""
+            )}
+            <Link to='/forgetPassword' className="mt-6 block hover:text-active">Forget Password ?</Link>
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-active hover:bg-green-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            Your email
-          </label>
-          <input
-            value={LoginForm.values.email}
-            onChange={LoginForm.handleChange}
-            onBlur={LoginForm.handleBlur}
-            type="email"
-            id="email"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-active focus:border-active block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          />
-          {LoginForm.touched.email && LoginForm.errors.email ? (
-            <p className="text-red-600">{LoginForm.errors.email}</p>
-          ) : (
-            ""
-          )}
-        </div>
-
-        <div className="mb-5">
-          <label
-            htmlFor="password"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Your password
-          </label>
-          <input
-            value={LoginForm.values.password}
-            onChange={LoginForm.handleChange}
-            onBlur={LoginForm.handleBlur}
-            type="password"
-            id="password"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-active focus:border-active block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          />
-          {LoginForm.touched.password && LoginForm.errors.password ? (
-            <p className="text-red-600">{LoginForm.errors.password}</p>
-          ) : (
-            ""
-          )}
-          <Link to='/forgetPassword' className="mt-6 block hover:text-active">Forget Password ?</Link>
-        </div>
-        <button
-          type="submit"
-          className="text-white bg-active hover:bg-green-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Submit
-        </button>
-      </form>
+            Submit
+          </button>
+        </form>
+    </div>
     </div>
   );
 }
